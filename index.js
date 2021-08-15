3e31e90f76de62d46db96c447ea16b1ae1a82963
mingyeom <mingyeom119@naver.com>
2021-08-15 19:12:24 +0900

    test2

--- index.js
+++ index.js
@@ -12,8 +12,6 @@ mongoose.connect(config.mongoURI,{
 .then(()=>console.log('MongoDB Connected...'))
 .catch(err => console.log(err));
 
-ddd
-
 const app = express();
 app.get('/', (req, res) => res.send("Hello, World!"));
 
